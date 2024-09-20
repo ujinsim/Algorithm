b6e28e2133ca11e3f93ddb790e9526c2698c159b
ujinsim <98771235+ujinsimSS@users.noreply.github.com>
2024-09-20 15:13:04 +0900

    [Bronze II] Title: 수빈이와 수열, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10539. 수빈이와 수열/수빈이와 수열.py
+++ 백준/Bronze/10539. 수빈이와 수열/수빈이와 수열.py
@@ -1,6 +1,6 @@
 #자기 배열 인덱스까지 더해서 나누기 자기 인덱스 
 #반대로 하면 위 곱하기 자기 인덱스 아래 빼기 자기 앞에 요소들
-
+#sum매서드 사용
 
 n = int(input(""))
 
@@ -9,9 +9,7 @@ array = list(map(int, input("").split()))
 result = [] 
 minus = 0  # 이전 항까지의 합을 저장
 for i in range(n):
-        value = array[i] * (i + 1) - minus
-    
-        minus += value  
+        value = array[i] * (i + 1) - sum(result)
         result.append(value)  
 
 
