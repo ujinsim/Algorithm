fce89d4773186cabf0db87f433db1acb973aab4c
ujinsim <syj11243@naver.com>
2025-03-10 13:43:26 +0900

    [Silver IV] Title: 스택 2, Time: 912 ms, Memory: 176004 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.js
@@ -0,0 +1,43 @@
+const [N, ...arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const stack = [];
+const results = [];
+
+for (let i = 0; i < arr.length; i++) {
+  let query = arr[i];
+  let [cmd, value] = query.split(" ");
+
+  switch (cmd) {
+    case "1":
+      stack.push(parseInt(value));
+      break;
+    case "2":
+      if (stack.length > 0) {
+        results.push(stack.pop());
+      } else {
+        results.push(-1);
+      }
+      break;
+    case "3":
+      results.push(stack.length);
+      break;
+    case "4":
+      results.push(stack.length === 0 ? 1 : 0);
+      break;
+    case "5":
+      if (stack.length > 0) {
+        results.push(stack[stack.length - 1]);
+      } else {
+        results.push(-1);
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+console.log(results.join("\n"));
