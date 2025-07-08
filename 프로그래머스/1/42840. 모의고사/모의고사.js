95ff1572489200c7410398f7659d3ae7f9bfe284
ujinsim <syj11243@naver.com>
2025-07-09 00:22:22 +0900

    [level 1] Title: 모의고사, Time: 11.83 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,28 +1,30 @@
 function solution(answers) {
-  const one = [1, 2, 3, 4, 5];
-  const two = [2, 1, 2, 3, 2, 4, 2, 5];
-  const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  const ways = [one, two, three];
+  
+  const ways = [[1, 2, 3, 4, 5],[2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] ]
+  const counts = new Array(ways.length).fill(0)
+  
+  for(i in ways) {
+        for (j in answers){
+            if(answers[j] == ways[i][j%ways[i].length]) {
+                counts[i] = counts[i]+1
+            }
+        }
 
-  const count = {};
-
-  for (let way = 0; way < ways.length; way++) {
-    for (let i = 0; i < answers.length; i++) {
-      if (answers[i] === ways[way][i % ways[way].length]) {
-        if (count[way]) count[way] += 1;
-        else count[way] = 1;
-      }
-    }
   }
+    
+    const result = []
 
-  const max = Math.max(...Object.values(count));
-
-  const result = [];
-  for (let [key, value] of Object.entries(count)) {
-    if (value === max) {
-      result.push(Number(key) + 1);
+for(let i=0; i<counts.length; i++) {
+    let max=Math.max(...counts)
+    
+    if(max == counts[i]){
+        result.push(i+1)
     }
-  }
-
-  return result.sort((a, b) => a - b); 
+    
+    
 }
+  
+  return result
+    
+}
+
