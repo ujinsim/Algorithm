e429239b7676c923aa7b0c77e62f09cdf3c44c06
ujinsim <syj11243@naver.com>
2025-07-09 01:15:56 +0900

    [level 2] Title: 올바른 괄호, Time: 29.75 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,22 +1,18 @@
 function solution(s){
-    let answer;
-    let stack = [];
-    for (let i=0; i<s.length; i++) {
-        if(s[i]== ')'&& stack[stack.length-1]=='('){
-            stack.pop()
-        }
-       
-       else if (s[i] == '(' || s[i]==')') {
+   const stack = []
+   
+   for(let i=0; i<s.length; i++) {
+       if(s[i] =="("){
            stack.push(s[i])
        }
-    }
-    if(stack.length == 0) {
-        answer = true;
-    }
-    else 
-        answer = false ;
-        
-    
-
-    return answer;
+       else {
+           if(stack.length > 0 ) {
+               stack.pop()
+           }
+           else {
+               return false
+           }
+       }
+   }
+    return stack.length>0 ? false : true
 }
