8ceb1002edc8bcfdfb4699d8575f51fb7cb133cc
ujinsim <syj11243@naver.com>
2025-07-09 01:02:06 +0900

    [level 1] Title: 실패율, Time: 359.24 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?gad_source=1&gad_campaignid=22199869887&gbraid=0AAAAAC_c4nBUvVghph3s5KLGeXC2e198r&gclid=CjwKCAjw4K3DBhBqEiwAYtG_9EqYxJQxUm6xBaNP7qat1OBEjefGSjHIVTHOpUA-59eOT76xgRH1oRoCSI4QAvD_BwE) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 40.4 MB, 시간: 462.01 ms
+메모리: 40.5 MB, 시간: 359.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 07일 19:32:46
+2025년 07월 09일 01:02:03
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,6 +1,7 @@
 function solution(N, stages) {
   const arr1 = new Array(N).fill(0);
   const arr2 = new Array(N).fill(0);
+    const result = []
 
   for (let i = 0; i < stages.length; i++) {
     const num = stages[i];
@@ -13,21 +14,15 @@ function solution(N, stages) {
     for (let i = 0; i < stages.length; i++) {
       if (stages[i] >= j + 1) {
         arr2[j] += 1;
+          
       }
+      
     }
+        result.push([j+1,arr1[j]/arr2[j]])
   }
+    
+    result.sort((a,b) => b[1]-a[1])
+    
 
-  const result = [];
-
-  for (let k = 0; k < N; k++) {
-    const fail = arr2[k] === 0 ? 0 : arr1[k] / arr2[k];
-    result.push([k + 1, fail]);
-  }
-
-  result.sort((a, b) => {
-    if (b[1] === a[1]) return a[0] - b[0];
-    return b[1] - a[1];
-  });
-
-  return result.map(r => r[0]);
+return result.map((a)=> a[0])
 }
