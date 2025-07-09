8ae3b78019a8e40da3bf8aea055877eb3e6d7328
ujinsim <syj11243@naver.com>
2025-07-09 10:50:57 +0900

    [level 2] Title: 괄호 회전하기, Time: 14.01 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,30 +1,37 @@
 function solution(s) {
-    const arr = s.split("");
-    let count = 0;
+  const arr = s.split("");
+  let count = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        const a = arr.shift();
-        arr.push(a);
-        if (isCorrect(arr)) {
-            count += 1;
+  for (let i = 0; i < arr.length; i++) {
+    const a = arr.shift();
+    arr.push(a);
+    if (isCorrect(arr)) {
+      count += 1;
+    }
+  }
+
+  function isCorrect(a) {
+    const stack = [];
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === "[" || a[i] === "(" || a[i] === "{") {
+        stack.push(a[i]);
+      } else {
+        const top = stack[stack.length - 1];
+        if (
+          (a[i] === ")" && top === "(") ||
+          (a[i] === "]" && top === "[") ||
+          (a[i] === "}" && top === "{")
+        ) {
+          stack.pop();
+        } else {
+          return false;
         }
+      }
     }
 
-    function isCorrect(a) {
-        const stack = [];
-        for (let j = 0; j < a.length; j++) {
-            if (a[j] === "[" || a[j] === "(" || a[j] === "{") {
-                stack.push(a[j]);
-            } else {
-                const top = stack[stack.length - 1];
-                if (a[j] === ")" && top === "(") stack.pop();
-                else if (a[j] === "]" && top === "[") stack.pop();
-                else if (a[j] === "}" && top === "{") stack.pop();
-                else return false; 
-            }
-        }
-        return stack.length === 0;
-    }
+    return stack.length === 0;
+  }
 
-    return count;
+  return count;
 }
