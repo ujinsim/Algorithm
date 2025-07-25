a6354bbcaebd9b31c38bb906e717fa87ff893f38
ujinsim <syj11243@naver.com>
2025-07-25 13:05:48 +0900

    [level 2] Title: 미로 탈출, Time: 70.15 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -1,51 +1,55 @@
 function solution(maps) {
-  const n = maps.length;
+    const n = maps.length;
   const m = maps[0].length;
-  const dx = [1, -1, 0, 0];
-  const dy = [0, 0, 1, -1];
-
-  let S, L, E;
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (maps[i][j] === "S") S = [i, j];
-      if (maps[i][j] === "L") L = [i, j];
-      if (maps[i][j] === "E") E = [i, j];
-    }
-  }
-
-  function bfs(start, target) {
-    const visited = Array.from({ length: n }, () => Array(m).fill(false));
-    const queue = [[start[0], start[1], 0]];
-    visited[start[0]][start[1]] = true;
-
-    while (queue.length) {
-      const [x, y, dist] = queue.shift();
-
-      if (maps[x][y] === target) return dist;
-
-      for (let dir = 0; dir < 4; dir++) {
-        const nx = x + dx[dir];
-        const ny = y + dy[dir];
-
-        if (
-          nx >= 0 && ny >= 0 &&
-          nx < n && ny < m &&
-          !visited[nx][ny] &&
-          maps[nx][ny] !== "X"
-        ) {
-          visited[nx][ny] = true;
-          queue.push([nx, ny, dist + 1]);
+    const dx = [1,-1,0,0]
+    const dy = [0,0,1,-1]
+    
+    let S,L,E; 
+    
+    for(let i=0; i<maps.length; i++){
+        for(let j=0; j<m; j++){
+            console.log(maps[i][j])
+            if(maps[i][j] == "S") S = [i,j]
+            if(maps[i][j] == "L") L = [i,j]
+            if(maps[i][j] == "E") E = [i,j]
         }
-      }
+    }
+    
+    function bfs(start, target){
+        const visited = Array.from({length:n}, ()=> Array(m).fill(false))
+       const queue = [[start[0], start[1], 0]]
+         visited[start[0]][start[1]] = true;
+        const x = start[0]
+        const y = start[1]
+        let count = 0
+        
+        while(queue.length > 0){
+           const [x, y, dist] = queue.shift();
+            if (maps[x][y] === target) return dist;
+            
+            for(let i=0; i<4; i++){
+                const nx = x+dx[i]
+                const ny = y+dy[i]
+                
+                if(nx >= 0 && ny >= 0 &&  nx < n && ny < m && !visited[nx][ny] && maps[nx][ny] !== "X"){
+                     queue.push([nx, ny, dist + 1]);
+                    visited[nx][ny] = true
+                }
+                
+            }
+            
+        }
+        return -1
+ 
+ 
+    }
+     const toLever = bfs(S, "L");
+    const toExit = bfs(L, "E");
+  
+    if (toLever === -1 || toExit === -1) {
+        return -1;
     }
 
-    return -1;
-  }
-
-  const toLever = bfs(S, "L");
-  const toExit = bfs(L, "E");
-
-  if (toLever === -1 || toExit === -1) return -1;
-  return toLever + toExit;
+    return toLever + toExit;
+   
 }
