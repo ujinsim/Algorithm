d0155c75fc2c27bd181ba2cb39d2380d28377d3a
ujinsim <syj11243@naver.com>
2025-08-09 01:37:30 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,36 +1,24 @@
 function solution(n, computers) {
-    
-    const graph = {};
-    const visited = Array(n).fill(false)
-    for (let i = 0; i < n; i++) {
-        graph[i] = [];
-    for (let j = 0; j < n; j++) {
-      if (i !== j && computers[i][j] === 1) {
-        graph[i].push(j);
-      }
-    }
-  }
-    
-    function dfs(node) {
-            visited[node] = true;
-            for(let next of graph[node]){
-                 if (!visited[next]) {
-                dfs(next)
-                 }
-            }
-        
-    }
-        let count = 0
-        
-        for(let i=0; i<n; i++) {
-            
-            if(!visited[i]){
-                dfs(i)
-                count++;
-            }
-        
-}
-    
-    return count;
-}
+    const isVisited = new Array(n).fill(0);
+    let result = 0;
 
+    for (let i = 0; i < n; i++) {
+        if (isVisited[i] === 0) {  // 방문 안 했으면 새로운 네트워크 시작
+            const stack = [i];
+            isVisited[i] = 1;
+            result++;
+            while (stack.length > 0) {
+                const current = stack.pop();
+                const connections = computers[current];
+                for (let j = 0; j < connections.length; j++) {
+                    if (isVisited[j] === 0 && connections[j] === 1) {
+                        stack.push(j);
+                        isVisited[j] = 1;
+                    }
+                }
+            }
+            
+        }
+    }
+    return result;
+}
