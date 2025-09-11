5ba1a823355c5083d2ddf437afdf9542650136de
ujinsim <syj11243@naver.com>
2025-09-11 15:37:19 +0900

    [level 3] Title: 정수 삼각형, Time: 26.32 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,17 +1,11 @@
 function solution(triangle) {
   const n = triangle.length;
-  const memo = Array.from({ length: n }, (_, i) =>
-    Array(triangle[i].length).fill(undefined)
-  );
-
-  function dfs(i, j) {
-    if (i === n - 1) return triangle[i][j];       
-    if (memo[i][j] !== undefined) return memo[i][j];
-
-    const left  = dfs(i + 1, j);
-    const right = dfs(i + 1, j + 1);
-    return (memo[i][j] = triangle[i][j] + Math.max(left, right));
+    
+  for(let i=n-1; i>=0; i--){
+      for(let j=0; j<i; j++){
+          triangle[i-1][j] = Math.max(triangle[i][j], triangle[i][j+1]) + triangle[i-1][j]
+      }
   }
-
-  return dfs(0, 0);
+    return Number(triangle[0].join())
+  
 }
