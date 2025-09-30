1b0cd2bad5b11fb246b6ce5d18ae55af136920eb
ujinsim <syj11243@naver.com>
2025-09-30 13:45:11 +0900

    [level 2] Title: 배달, Time: 3.53 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?gad_source=1&gad_campaignid=22356298761&gbraid=0AAAAAC_c4nD5xisQUcD_LPFG-ZrnFgC3S&gclid=Cj0KCQjw3OjGBhDYARIsADd-uX6fzGHTq_3FAUe1EqLAMYbfbfr0pwcXtJSa3lXqgvGc0yi3RXS6icoaAjVvEALw_wcB) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 1.56 ms
+메모리: 35 MB, 시간: 3.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 13일 17:32:24
+2025년 09월 30일 13:45:09
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,63 +1,45 @@
 function solution(N, road, K) {
-    // 완조니 다익스트라 알고리즘 
-
-    // K를 넘지하는 길이로 갈수 있는 경로를 찾기
-    // 최단으로 다 찾고 안넘으면 filter해서 주기 
+  const graph = new Map()
+  const costs = {}
+  const visited = new Set()
+  let result = 0
+  
+  for (const [a, b, c] of road) {
+    if (!graph.has(a)) graph.set(a, []);
+    if (!graph.has(b)) graph.set(b, []);
+    graph.get(a).push([b, c]);
+    graph.get(b).push([a, c]);
+  }
     
-    //graph, cost, prev, visited 
+for (let i = 1; i <= N; i++) costs[i] = Infinity;
+  costs[1] = 0;
+  
+  while(visited.size < N){
+    let current = null
+    let currentCost = Infinity
     
-    const graph = {}
-    const cost = {}
-    const visited = new Set()
-    const prev = {}
-    const start = 1
-    let result = 0
-    
-    for (let i = 0; i < road.length; i++) {
-        const [from, to, weight] = road[i];
-        if (!graph[from]) graph[from] = {};
-        if (!graph[to]) graph[to] = {};
-        if (!graph[from][to] || graph[from][to] > weight) graph[from][to] = weight;
-        if (!graph[to][from] || graph[to][from] > weight) graph[to][from] = weight;
-
+    for(let i = 1; i <= N; i++){
+      if(!visited.has(i) && costs[i] < currentCost){
+        current = i
+        currentCost = costs[i]
+      }
     }
     
+    if (current === null) break;
     
-    for (let i = 1; i <= N; i++) {
-        cost[i] = Infinity; 
-        prev[i] = null;     
+    const neighbors = graph.get(current) || [];
+    for (const [neighbor, w] of neighbors) {
+      const newCost = costs[current] + w;
+      if (newCost < costs[neighbor]) {
+        costs[neighbor] = newCost;
+      }
     }
-    cost[start] = 0;
-    
-    while (visited.size < N) { 
-        // 방문안한거에서 가장 작은거 찾기
-        let current = null;  
-        let currentCost = Infinity; 
-        
-        for (let node in cost) {
-            if (!visited.has(Number(node)) && cost[node] < currentCost) {
-                current = Number(node);
-                currentCost = cost[node];
-            }
-        }
-        
-        // 더 이상 방문할 노드가 없으면 종료
-        if (current === null) break;
-        
-        // 찾은거 주변 비용 업데이트 적다면
-        for (let neighbor in (graph[current] || {})) { 
-            const newCost = cost[current] + graph[current][neighbor];
-            if (newCost < cost[neighbor]) { 
-                cost[neighbor] = newCost;
-                prev[neighbor] = current;
-            }
-        }
-        visited.add(current);
-    }
-    
-    for (let node in cost) {
-    if (cost[node] <= K) result++;
+    visited.add(current)
+  }
+  
+ for (let i = 1; i <= N; i++) {
+  if (costs[i] <= K) result++;
 }
-
-    return result;
+    
+  return result
 }
