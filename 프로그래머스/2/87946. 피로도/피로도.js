937bce16b3164eae7d87d6134d7abeb13c2a78cb
ujinsim <syj11243@naver.com>
2025-10-01 15:38:16 +0900

    [level 2] Title: 피로도, Time: 8.31 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,21 +1,22 @@
 function solution(k, dungeons) {
-  const n = dungeons.length;
-  const visited = Array(n).fill(false);
-
-  let answer = 0;
-
-  function dfs(curK, count) {
-    answer = Math.max(answer, count);
-
-    for (let i = 0; i < n; i++) {
-      const [need, cost] = dungeons[i];
-      if (!visited[i] && curK >= need) {
-        visited[i] = true;
-        dfs(curK - cost, count + 1);
-        visited[i] = false; 
+    let result = 0
+    const n = dungeons.length
+    const visited = Array(n).fill(false);
+  
+    function dfs(curK,count){
+        result = Math.max(result, count);
+      
+      for(let i=0; i<n; i++){
+        const [p,s] = dungeons[i]
+          if(curK >= p && !visited[i]){
+            visited[i] = true
+            dfs(curK-s,count+1)
+            visited[i] = false
+        }
       }
     }
-  }
-  dfs(k, 0);
-  return answer;
+    
+    dfs(k,0)
+    
+    return result
 }
