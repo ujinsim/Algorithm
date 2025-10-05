af11a4cb630d315ccf8035ff2b469a3f5ff920ed
ujinsim <syj11243@naver.com>
2025-10-05 13:56:50 +0900

    [level 2] Title: 할인 행사, Time: 91.23 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,34 +1,35 @@
 function solution(want, number, discount) {
-    let count = 0;
-    
-    const want_num = new Map()
-    
-    for(let i=0; i<want.length; i++){
-        want_num.set(want[i], number[i])
+  
+  const want_map = new Map()
+  let result = 0
+  
+  for(let i=0; i<want.length; i++){
+    if(!want_map.has(want[i])){
+      want_map.set(want[i], number[i])
     }
-    
-    for(let i=0; i<discount.length-9; i++){
-        let currentWindow = new Map();
-        for(let j=i; j<10+i; j++){
-            const item = discount[j]
-            currentWindow.set(item,(currentWindow.get(item) || 0) + 1)
-            
-            if (isMatch(want_num, currentWindow)){
-                count ++ ;
-            }
-            
-        }
-    }
-        return count
-    
-    function isMatch(want_num, currentWindow){
-        for(let [item, qty] of want_num){
-            if ((currentWindow.get(item) || 0) < qty) {
-        return false;
+  }
+  
+  for(let i=0; i<discount.length-9; i++){
+    let discount_map = new Map()
+    for(let j=i; j<10+i; j++){
+      const item = discount[j]
+      discount_map.set(item,(discount_map.get(item)||0)+1)
+      if(isMatch(want_map, discount_map)){
+        result++
       }
-        }
-        return true
-        
     }
-
+    
+  }
+  
+  function isMatch(want_map, discount_map){
+    for(let [item, qty] of want_map){
+      if((discount_map.get(item) || 0 ) < qty){
+        return false
+      }
+    }
+    return true
+  }
+  return result
+  
+  
 }
