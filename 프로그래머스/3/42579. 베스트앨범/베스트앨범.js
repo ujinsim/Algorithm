a30eaa7c1823ce8534ea71fe63957e45edc82b11
ujinsim <syj11243@naver.com>
2025-10-05 14:30:15 +0900

    [level 3] Title: 베스트앨범, Time: 0.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,36 +1,32 @@
 function solution(genres, plays) {
-  const genreMap = new Map(); 
-  const genreTotal = new Map(); 
+  const genres_map = new Map();
+  const count_by_genres = new Map();
+  const result = [];
 
   for (let i = 0; i < genres.length; i++) {
     const genre = genres[i];
     const play = plays[i];
 
-    if (!genreMap.has(genre)) {
-      genreMap.set(genre, []);
-      genreTotal.set(genre, 0);
+    genres_map.set(genre, (genres_map.get(genre) || 0) + play);
+
+    if (!count_by_genres.has(genre)) count_by_genres.set(genre, []);
+    count_by_genres.get(genre).push([i, play]);
+  }
+
+  const sorted = new Map([...genres_map.entries()].sort((a, b) => b[1] - a[1]));
+
+  const sorted_genre_count = new Map(
+    [...count_by_genres.entries()].map(([genre, arr]) => [
+      genre,
+      [...arr].sort((a, b) => (b[1] - a[1]) || (a[0] - b[0])),
+    ])
+  );
+
+  for (const [genre] of sorted) {
+    const list = sorted_genre_count.get(genre);
+    for (let i = 0; i < Math.min(2, list.length); i++) {
+      result.push(list[i][0]);
     }
-
-    genreMap.get(genre).push([i, play]);
-    genreTotal.set(genre, genreTotal.get(genre) + play);
   }
-
-  const sortedGenres = [...genreTotal.entries()]
-    .sort((a, b) => b[1] - a[1])
-    .map(entry => entry[0]);
-
-  const result = [];
-
-
-  for (const genre of sortedGenres) {
-    const songs = genreMap.get(genre);
-    songs.sort((a, b) => {
-      if (b[1] === a[1]) return a[0] - b[0]; 
-      return b[1] - a[1];
-    });
-
-    result.push(...songs.slice(0, 2).map(song => song[0]));
-  }
-
-  return result
+  return result;
 }
