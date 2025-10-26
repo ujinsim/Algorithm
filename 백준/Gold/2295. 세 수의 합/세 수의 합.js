ae258aef6b62f708d2ff6fe6cb0ed85adb571679
ujinsim <syj11243@naver.com>
2025-10-26 16:40:12 +0900

    [Gold IV] Title: 세 수의 합, Time: 192 ms, Memory: 30992 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.js
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.js
@@ -1,31 +1,23 @@
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf8').trim().split('\n').map(Number);
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const n = input[0];
-const arr = input.slice(1).sort((a, b) => a - b);
+const n = Number(input[0]);
+const arr = input.slice(1).map(Number).sort((a, b) => a - b);
 
-const twoSum = [];
+const sumSet = new Set();
+
 for (let i = 0; i < n; i++) {
   for (let j = i; j < n; j++) {
-    twoSum.push(arr[i] + arr[j]);
+    sumSet.add(arr[i] + arr[j]);
   }
 }
-twoSum.sort((a, b) => a - b);
 
 for (let k = n - 1; k >= 0; k--) {
-  for (let i = 0; i < k; i++) {
-    const target = arr[k] - arr[i];
-
-    let left = 0;
-    let right = twoSum.length - 1;
-    while (left <= right) {
-      const mid = Math.floor((left + right) / 2);
-      if (twoSum[mid] === target) {
-        console.log(arr[k]);
-        process.exit(0);
-      }
-      if (twoSum[mid] < target) left = mid + 1;
-      else right = mid - 1;
+  for (let i = 0; i <= k; i++) {
+    const diff = arr[k] - arr[i];
+    if (sumSet.has(diff)) {
+      console.log(arr[k]);
+      process.exit(0);
     }
   }
 }
