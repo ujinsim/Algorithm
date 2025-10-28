4c8b18698962a88139b701f5cfe298ccecfd4d8c
ujinsim <syj11243@naver.com>
2025-10-28 19:24:34 +0900

    [level 2] Title: 프로세스, Time: 0.50 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,24 +1,30 @@
 function solution(priorities, location) {
-    let queue = priorities.map((priority, index) => {
-        return { priority, index }; 
+    const queue = priorities.map((priority, index) => {
+        return { priority, index };
     });
-
-    let count = 0;
-
-    while (queue.length) {
-        const current = queue.shift(); 
-
-   
-        const hasHigher = queue.some(job => job.priority > current.priority);
-
-        if (hasHigher) {
-            queue.push(current); 
-        } else {
-            count++; 
-
-            if (current.index === location) {
-                return count; 
-            }
+  
+    let count = 0
+    
+    const result = []
+    while(queue.length !==0){
+      
+      const current = queue.shift()
+      const highValue = queue.some(doc=> doc.priority > current.priority)
+      
+      if(highValue){
+        queue.push(current)
+      }
+      else{
+        count ++
+        
+        if(current.index === location){
+          return count 
         }
+      }
+      
+      
     }
+  return 
 }
+
+console.log(solution([2, 1, 3, 2],2))
