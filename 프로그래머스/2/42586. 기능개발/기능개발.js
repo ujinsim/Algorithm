b5d8701a90e78ec11076cbfb78d53506a45bfa3d
ujinsim <syj11243@naver.com>
2025-11-19 15:23:57 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,19 +1,21 @@
-function solution(progresses, speeds) {
-    let answer = [];
-
-    while (progresses.length) {
-        progresses = progresses.map((p, i) => p + speeds[i]);
-        let count = 0
-        console.log( progresses)
-        while(progresses[0]>=100) {
-            progresses.shift()
-            speeds.shift()
-            count++;
-        }
-        if(count != 0){
-            answer.push(count)
-        }
-    }
-
-    return answer;
+function solution(progresses,speeds){
+  let count = 0
+  const n = progresses.length
+  const result =[]
+  
+  while(progresses.length > 0){
+      for(let i=0; i<progresses.length; i++){
+          progresses[i] += speeds[i]
+      }
+      if(progresses[0] >= 100){
+          let count = 0
+          while(progresses[0] >= 100){
+              progresses.shift()
+              speeds.shift();
+              count+=1
+          }
+          result.push(count)
+      }
+  }
+    return result
 }
