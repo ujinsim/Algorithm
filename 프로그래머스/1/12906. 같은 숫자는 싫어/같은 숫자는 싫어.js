82de423e52ba5c6cbabbe7feec2a59437dd8ffbc
ujinsim <syj11243@naver.com>
2025-11-19 15:15:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.78 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,12 @@
 function solution(arr){
-    const newArr = [arr[0]]
-    
-    for(let i=1; i<arr.length; i++){
-        if(newArr[newArr.length-1] !== arr[i]){
-            newArr.push(arr[i])
-        }
-    }
-    
-    return newArr
+ const stack = []
+ stack.push(arr[0])
+ 
+ for(let i=1; i<arr.length; i++){
+     const value = stack[stack.length-1]
+     if(arr[i] !== value){
+         stack.push(arr[i])
+     }
+ }
+ return stack
 }
