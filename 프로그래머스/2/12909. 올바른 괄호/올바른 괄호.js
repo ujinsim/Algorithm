3474b8f91e7608744c6ec78748981f1bcee3fbdb
ujinsim <syj11243@naver.com>
2025-11-19 15:28:22 +0900

    [level 2] Title: 올바른 괄호, Time: 6.31 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,24 +1,18 @@
 function solution(s){
-    // "(" 이거면 넣기 or 스택 마지막이 ")" 이거면 요걸 pop해줌
-    // ")"이거인데 스택 요소 없으면 false
-    
-    
-    const stack = []
-    
-    for(let i=0; i<s.length; i++){
-        let value = s[i]
-        
-        if(value == "("){
-            stack.push(value)
-        }
-        if(value == ")" && stack.length === 0){
-            return false
-        }
-        
-        if(value == ")" && stack[stack.length-1] == "("){
-            stack.pop()
-        }
-    }
-    
-    return stack.length > 0 ? false : true
+   const stack = []
+   
+   for(let i=0; i<s.length; i++){
+       if(s[i] == "("){
+           stack.push(s[i])
+       }
+       if(s[i] == ")"){
+           if(stack[stack.length-1] == "("){
+               stack.pop()
+           }
+           else{
+               stack.push(")")
+           }
+       }
+   }
+   return stack.length > 0 ? false : true
 }
