2b168e32d950337a134324e1a9707914278cfed6
ujinsim <syj11243@naver.com>
2025-11-19 14:55:12 +0900

    [level 3] Title: 베스트앨범, Time: 5.23 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,32 +1,44 @@
 function solution(genres, plays) {
-  const genres_map = new Map();
-  const count_by_genres = new Map();
-  const result = [];
 
-  for (let i = 0; i < genres.length; i++) {
-    const genre = genres[i];
-    const play = plays[i];
-
-    genres_map.set(genre, (genres_map.get(genre) || 0) + play);
-
-    if (!count_by_genres.has(genre)) count_by_genres.set(genre, []);
-    count_by_genres.get(genre).push([i, play]);
+ const map = new Map()
+ const count = new Map()
+ const result = []
+ 
+ for(let i=0; i<genres.length; i++){
+    const genre = genres[i]
+    const play = plays[i]
+    if(!map.has(genre)) map.set(genre,[])
+     map.get(genre).push([play,i])
+ }
+    
+ for(const [key, value] of map){
+    value.sort((a,b)=> b[0]-a[0])
+ }   
+ 
+  for(let i=0; i<genres.length; i++){
+    const genre = genres[i]
+    const play = plays[i]
+    if(!count.has(genre)) count.set(genre,0)
+     count.set(genre, count.get(genre)+play)
+ }
+    
+ for(const [key, value] of map){
+     console.log(key, value)
+ }
+    
+ 
+  const countArr = Array.from(count)
+  countArr.sort((a,b)=> b[1]-a[1])
+    
+  for(let i=0; i<countArr.length; i++){
+      const genre = countArr[i][0]
+      const sortedCounts = map.get(genre)
+      
+      for(let j=0; j < Math.min(sortedCounts.length, 2); j++){
+          result.push(sortedCounts[j][1])
+      }
   }
-
-  const sorted = new Map([...genres_map.entries()].sort((a, b) => b[1] - a[1]));
-
-  const sorted_genre_count = new Map(
-    [...count_by_genres.entries()].map(([genre, arr]) => [
-      genre,
-      [...arr].sort((a, b) => (b[1] - a[1]) || (a[0] - b[0])),
-    ])
-  );
-
-  for (const [genre] of sorted) {
-    const list = sorted_genre_count.get(genre);
-    for (let i = 0; i < Math.min(2, list.length); i++) {
-      result.push(list[i][0]);
-    }
-  }
-  return result;
+    return result
 }
+
+
