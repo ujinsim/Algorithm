a73b5bde25c366b2a4fcecb628e37ac322662058
ujinsim <syj11243@naver.com>
2025-11-22 19:59:13 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,12 +1,13 @@
 function solution(array, commands) {
-    const result = []
+  const result = []
     
-    for(c of commands){
-        const value = array.slice(c[0]-1, c[1])
-        
-        const sortedValue = value.sort((a,b)=>a-b)
-        result.push(sortedValue[c[2]-1])
-    }
-    
+  for(let i=0; i<commands.length; i++){
+      const [I,J,K] = commands[i]
+      const value = []
+      for(let j=I-1; j<J; j++){
+         value.push(array[j])
+      }
+      result.push(value.sort((a,b)=> a-b)[K-1])
+  }
     return result
 }
