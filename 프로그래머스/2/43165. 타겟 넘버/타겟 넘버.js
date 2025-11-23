a450fb6903436dc35e4950124b7a558143418534
ujinsim <syj11243@naver.com>
2025-11-23 21:21:46 +0900

    [level 2] Title: 타겟 넘버, Time: 45.16 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,23 @@
 function solution(numbers, target){
- // 모든 경우이기에 DFS
-  let count = 0
-  let depth = numbers.length 
-  
-  function dfs(sum, n){
-    if(n === depth){
-      if(sum == target){
-        count ++
-      }
-      return
+
+    let result = 0
+    
+    function dfs(count, current){
+        
+        if(count === numbers.length && target == current){
+            result +=1
+            return
+        }
+        if(numbers[count]){
+            dfs(count+1, current+numbers[count])
+            dfs(count+1, current-numbers[count])
+        }
     }
     
-    dfs(sum-numbers[n], n+1)
-    dfs(sum+numbers[n], n+1)
-  }
-  
-  dfs(0,0)
-  return count 
+    
+    dfs(0,0)
+    
+    return result
+
+    
 }
