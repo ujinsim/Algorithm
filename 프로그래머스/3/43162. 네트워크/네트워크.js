6985645422d94bbdba6ef3af9538ad4b341e76d4
ujinsim <syj11243@naver.com>
2025-11-24 16:37:22 +0900

    [level 3] Title: 네트워크, Time: 1.13 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,45 +1,31 @@
 function solution(n, computers){
-  const tree = new Map()
-  let count = 0
-  
-  for(let i=0; i<computers.length; i++){
-    for(let j=0; j<computers[i].length; j++){
-      if(j !== i && computers[i][j] === 1){
-        if(!tree.has(i)) tree.set(i,[])
-        tree.get(i).push(j)
-     
-        
-      }
+    const graph = new Map()
+    const visited = new Array(n).fill(false)
+    
+    for (let i = 0; i < n; i++) {
+        graph.set(i, []);
+        for (let j = 0; j < n; j++) {
+            if (i !== j && computers[i][j] === 1) {
+                graph.get(i).push(j);
+            }
+        }
     }
-  }
-  
-  const visited = new Set()
-  const queue = []
-
-  for(let i=0; i<n; i++){
-    if(!visited.has(i)){
-      count++
-      queue.push(i)
-      visited.add(i)
-      bfs()
+    let count = 0
+    
+    function dfs(node) {
+        visited[node] = true;
+        const neighbors = graph.get(node) || [];
+        for (const next of neighbors) {
+            if (!visited[next]) dfs(next);
+        }
     }
-  }
-  
- function bfs(){
-   while(queue.length > 0){
-   const value = queue.shift()
-   const neighbors = tree.get(value) || []
-
-    for(let i=0; i<neighbors.length; i++){
-      if(!visited.has(neighbors[i])){
-        queue.push(neighbors[i])
-        visited.add(neighbors[i])
-      }
+    
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            count++;
+        }
     }
-}
- }
-
-  
-
-  return count
+    
+    return count 
 }
