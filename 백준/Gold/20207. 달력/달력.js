67bf23609309d9634e47fc9c96f78916fab8bc0c
ujinsim <syj11243@naver.com>
2025-12-19 13:33:41 +0900

    [Gold V] Title: 달력, Time: 100 ms, Memory: 9760 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.js
+++ 백준/Gold/20207. 달력/달력.js
@@ -2,21 +2,27 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString();
 
 function solution(input) {
-  const lines = input.trim().split("\n");
+  const lines = input.trim().split('\n');
   const n = Number(lines[0]);
-  const calendar = new Array(367).fill(0);
+  const diff = new Array(368).fill(0);
 
   for (let i = 1; i <= n; i++) {
     const [start, end] = lines[i].split(' ').map(Number);
-    for (let j = start; j <= end; j++) {
-      calendar[j]++;
-    }
+    diff[start] += 1;
+    diff[end + 1] -= 1;
+  }
+
+  const calendar = new Array(367).fill(0);
+  let currentSum = 0;
+  for (let i = 1; i <= 366; i++) {
+    currentSum += diff[i];
+    calendar[i] = currentSum;
   }
 
   let totalArea = 0;
   let currentWidth = 0;
   let maxHeight = 0;
-  
+
   for (let i = 1; i <= 366; i++) {
     if (calendar[i] > 0) {
       currentWidth++;
