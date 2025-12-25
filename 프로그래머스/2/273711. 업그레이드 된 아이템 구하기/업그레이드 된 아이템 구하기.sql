f2fc99a7428b1d2415d3e87806eec1f3d0155d1c
ujinsim <syj11243@naver.com>
2025-12-26 07:36:08 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,9 @@
+select item_id, item_name, rarity
+from item_info
+where item_id in (
+    select t.item_id
+    from item_info i
+    join item_tree t on i.item_id = t.parent_item_id
+    where i.rarity = 'RARE'
+)
+order by item_id desc;
