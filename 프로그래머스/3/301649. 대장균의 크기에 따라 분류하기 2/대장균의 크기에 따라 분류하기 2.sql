104b6dd4b30cc91efaaf02ba1a2a15a3d4d6fdbf
ujinsim <syj11243@naver.com>
2025-12-31 16:00:47 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,1 +1,13 @@
--- 코드를 작성해주세요
+select id, 
+       case 
+        when per <= 0.25 then 'CRITICAL'
+        when per <= 0.50 then 'HIGH'
+        when per <= 0.75 then 'MEDIUM'
+        else "LOW"
+        end as colony_name
+
+
+from (select id, percent_rank() over (order by size_of_colony desc) as per
+from ecoli_data) as p
+order by id asc
+
