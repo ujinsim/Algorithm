7ece83e26b8fe2d9f77fc384c542e1428b15a97a
ujinsim <syj11243@naver.com>
2026-01-07 14:22:03 +0900

    [Gold IV] Title: 가장 먼 곳, Time: 2980 ms, Memory: 277212 KB -BaekjoonHub

--- 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.js
+++ 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.js
@@ -6,32 +6,37 @@ class MinHeap {
   constructor() {
     this.h = [];
   }
-  push(v) {
-    this.h.push(v);
-    let cur = this.h.length - 1;
-    while (cur > 0) {
-      let p = Math.floor((cur - 1) / 2);
-      if (this.h[p][0] <= this.h[cur][0]) break;
-      [this.h[p], this.h[cur]] = [this.h[cur], this.h[p]];
-      cur = p;
+
+  push(a) {
+    this.h.push(a);
+    let curr = this.h.length - 1;
+    while (curr > 0) {
+      let p = Math.floor((curr - 1) / 2);
+      if (this.h[p][0] <= this.h[curr][0]) break;
+      [this.h[p], this.h[curr]] = [this.h[curr], this.h[p]];
+      curr = p;
     }
   }
+
   pop() {
-    if (this.h.length === 0) return null;
-    if (this.h.length === 1) return this.h.pop();
-    const res = this.h[0];
+    if (this.h.length == 0) return null;
+    if (this.h.length == 1) return this.h.pop();
+
+    const rv = this.h[0];
     this.h[0] = this.h.pop();
-    let cur = 0;
-    while (cur * 2 + 1 < this.h.length) {
-      let l = cur * 2 + 1;
-      let r = cur * 2 + 2;
+    let curr = 0;
+
+    while (curr * 2 + 1 < this.h.length) {
+      let l = curr * 2 + 1;
+      let r = curr * 2 + 2;
       let s = l;
+
       if (r < this.h.length && this.h[r][0] < this.h[l][0]) s = r;
-      if (this.h[cur][0] <= this.h[s][0]) break;
-      [this.h[cur], this.h[s]] = [this.h[s], this.h[cur]];
-      cur = s;
+      if (this.h[curr][0] <= this.h[s][0]) break;
+      [this.h[s], this.h[curr]] = [this.h[curr], this.h[s]];
+      curr = s;
     }
-    return res;
+    return rv;
   }
 }
 
@@ -39,12 +44,13 @@ function solution(input) {
   const parseInput = input.split(`\n`);
   const N = Number(parseInput[0]);
   const [A, B, C] = parseInput[1].split(' ').map(Number);
-  const M = Number(parseInput[2]);
+  const M = Number(parseInput[1]);
   const nums = parseInput.slice(3).map(a => a.split(' ').map(Number));
   const graph = new Map();
 
   for (let i = 0; i < nums.length; i++) {
     const [n, v, c] = nums[i];
+
     if (!graph.has(n)) graph.set(n, []);
     graph.get(n).push([v, c]);
 
@@ -52,50 +58,55 @@ function solution(input) {
     graph.get(v).push([n, c]);
   }
 
-  function getDijkstra(startNode) {
-    const dist = new Array(N + 1).fill(Infinity);
+  // A위치에서 각 거리 구하기
+  // B위치에서 각 거리 구하기
+  // C 위치에서 각 거리 구하기
+  // 거리를 비교해서 각 가장 작은 거리중 가장 큰 거리를 가지고 있는 것의 노드번호를 뽑자
+
+  function dist(node) {
+    const distArr = new Array(N + 1).fill(Infinity);
+    distArr[node] = 0;
 
     const pd = new MinHeap();
-    pd.push([0, startNode]);
-    dist[startNode] = 0;
+    pd.push([0, node]);
 
     while (pd.h.length > 0) {
-      const popper = pd.pop();
+      const [c, n] = pd.pop();
 
-      if (!popper) break;
+      if (distArr[n] < c) continue;
 
-      const [n, c] = popper;
-      if (dist[c] < n) continue;
+      const neighbors = graph.get(n) || [];
+      for (let i = 0; i < neighbors.length; i++) {
+        const [nextNode, nextCount] = neighbors[i];
 
-      const neighbors = graph.get(c) || [];
-
-      for (const neighbor of neighbors) {
-        const [node, count] = neighbor;
-        if (dist[node] > n + count) {
-          dist[node] = n + count;
-          pd.push([dist[node], node]);
+        if (distArr[nextNode] > nextCount + c) {
+          distArr[nextNode] = nextCount + c;
+          pd.push([distArr[nextNode], nextNode]);
         }
       }
     }
-    return dist;
+
+    return distArr;
   }
 
-  const distA = getDijkstra(A);
-  const distB = getDijkstra(B);
-  const distC = getDijkstra(C);
+  const resultADist = dist(A);
+  const resultBDist = dist(B);
+  const resultCDist = dist(C);
 
-  let maxMinDist = -1;
-  let ansNode = 0;
+  let maxResult = 0;
+  let resultIdx = [];
 
-  for (let i = 1; i <= N; i++) {
-    const minDist = Math.min(distA[i], distB[i], distC[i]);
-    if (minDist > maxMinDist) {
-      maxMinDist = minDist;
-      ansNode = i;
+  for (let i = 1; i < N; i++) {
+    const value = Math.min(resultADist[i], resultBDist[i], resultCDist[i]);
+    if (value > maxResult) {
+      maxResult = value;
+      resultIdx = [i];
+    } else if (value == maxResult) {
+      resultIdx.push(i);
     }
   }
 
-  return ansNode;
+  return Math.min(...resultIdx);
 }
 
 console.log(solution(input));
