9b6e957da86bf8439afbb960f38bd5ec727b866a
ujinsim <syj11243@naver.com>
2026-01-08 06:10:45 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,8 @@
+set @hour = -1;
+
+select (@hour := @hour + 1) as hour, 
+    (select count(hour(datetime)) 
+     from animal_outs 
+     where hour(datetime) = @hour) as count
+from animal_outs
+where @hour < 23;
