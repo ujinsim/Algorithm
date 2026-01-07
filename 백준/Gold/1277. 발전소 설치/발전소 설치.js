e417cc41a1c469c5f06823968e3e831d1dea5b4b
ujinsim <syj11243@naver.com>
2026-01-07 13:35:15 +0900

    [Gold IV] Title: 발전소 설치, Time: 556 ms, Memory: 96748 KB -BaekjoonHub

--- 백준/Gold/1277. 발전소 설치/발전소 설치.js
+++ 백준/Gold/1277. 발전소 설치/발전소 설치.js
@@ -2,41 +2,6 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim();
 
-class MinHeap {
-  constructor() {
-    this.h = [];
-  }
-
-  push(value) {
-    this.h.push(value);
-    let curr = this.h.length - 1;
-    while (curr > 0) {
-      let p = Math.floor((curr - 1) / 2);
-      if (this.h[p][0] <= this.h[curr][0]) break;
-      [this.h[p], this.h[curr]] = [this.h[curr], this.h[p]];
-      curr = p;
-    }
-  }
-
-  pop() {
-    if (this.h.length === 0) return null;
-    if (this.h.length === 1) return this.h.pop();
-    const rv = this.h[0];
-    this.h[0] = this.h.pop();
-    let curr = 0;
-    while (curr * 2 + 1 < this.h.length) {
-      let l = curr * 2 + 1,
-        r = curr * 2 + 2,
-        s = l;
-      if (r < this.h.length && this.h[r][0] < this.h[l][0]) s = r;
-      if (this.h[curr][0] <= this.h[s][0]) break;
-      [this.h[curr], this.h[s]] = [this.h[s], this.h[curr]];
-      curr = s;
-    }
-    return rv;
-  }
-}
-
 function solution(input) {
   const lines = input.trim().split('\n');
   const [N, W] = lines[0].split(' ').map(Number);
@@ -50,54 +15,52 @@ function solution(input) {
   }
 
   const isConnected = Array.from({ length: N + 1 }, () => new Set());
-
-  for (let i = 0; i < connections.length; i++) {
-    const [n, m] = connections[i];
-
+  for (const [n, m] of connections) {
     isConnected[n].add(m);
     isConnected[m].add(n);
   }
 
-  const map = Array.from({ length: N + 1 }, () => []);
-
+  const adj = Array.from({ length: N + 1 }, () => []);
   for (let i = 1; i <= N; i++) {
-    for (let j = 1 + i; j <= N; j++) {
+    for (let j = i + 1; j <= N; j++) {
       if (isConnected[i].has(j)) {
-        map[i].push([j, 0]);
-        map[j].push([i, 0]);
+        adj[i].push([j, 0]);
+        adj[j].push([i, 0]);
       } else {
-        const dist = getDistance(nodes[i - 1], nodes[j - 1]);
-        if (dist <= M) {
-          map[i].push([j, dist]);
-          map[j].push([i, dist]);
+        const d = getDistance(nodes[i - 1], nodes[j - 1]);
+        if (d <= M) {
+          adj[i].push([j, d]);
+          adj[j].push([i, d]);
         }
       }
     }
   }
 
   const dist = new Array(N + 1).fill(Infinity);
-  const pd = new MinHeap();
+  const visited = new Array(N + 1).fill(false);
   dist[1] = 0;
-  pd.push([0, 1]);
 
-  while (pd.h.length) {
-    const [cost, node] = pd.pop();
+  for (let i = 0; i < N; i++) {
+    let currNode = -1;
+    let minCost = Infinity;
 
-    if (dist[node] < cost) continue;
+    for (let j = 1; j <= N; j++) {
+      if (!visited[j] && dist[j] < minCost) {
+        minCost = dist[j];
+        currNode = j;
+      }
+    }
 
-    const neighbors = map[node] || [];
+    if (currNode === -1) break;
+    visited[currNode] = true;
 
-    for (let i = 0; i < neighbors.length; i++) {
-      const [n, v] = neighbors[i];
-
-      if (dist[n] > cost + v) {
-        dist[n] = cost + v;
-        pd.push([dist[n], n]);
+    for (const [nextNode, weight] of adj[currNode]) {
+      if (dist[nextNode] > dist[currNode] + weight) {
+        dist[nextNode] = dist[currNode] + weight;
       }
     }
   }
 
-  return Math.floor(dist[N] * 1000);
+  return dist[N] === Infinity ? -1 : Math.floor(dist[N] * 1000);
 }
-
 console.log(solution(input));
