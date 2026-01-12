592fc1ea2c86eed48c44bb5e59fbc95381a5072c
ujinsim <syj11243@naver.com>
2026-01-12 14:08:26 +0900

    [Silver IV] Title: 카드2, Time: 208 ms, Memory: 28828 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.js
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -1,25 +1,20 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim();
 
+function solution(input) {
+  const num = Number(input);
+  const arr = Array.from({ length: num }, (v, a) => a + 1);
+  let head = 0;
 
-function solution(input){
-    const cards = Array.from({length:input}, (_,idx)=> idx+1)
-    let front = 0
+  while (head < arr.length - 1) {
+    // 버리기
+    head += 1;
 
-    while(cards.length - front > 1){
-        //버리기
-        front +=1
-        
-        //보내기
-        
-        cards.push(cards[front])
-        front+=1
-    }
-
-    return cards[front]
+    // 옮기기
+    arr.push(arr[head++]);
+  }
+  return arr[head];
 }
 
-
-
-console.log(solution(input))
+console.log(solution(input));
