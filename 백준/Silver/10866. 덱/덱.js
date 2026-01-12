960e11880f359311dc135d2f62933ec8c317612e
ujinsim <syj11243@naver.com>
2026-01-12 12:35:33 +0900

    [Silver IV] Title: 덱, Time: 164 ms, Memory: 13628 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.js
+++ 백준/Silver/10866. 덱/덱.js
@@ -6,69 +6,37 @@ function solution(input) {
   const parseInput = input.split('\n');
   const orders = parseInput.slice(1).map(a => a.split(' '));
 
-  const deque = {};
-
-  let headIdx = 0;
-  let tailIdx = 0;
-
+  const deque = [];
   const result = [];
 
   for (let i = 0; i < orders.length; i++) {
-    const [order, val] = orders[i];
-    const num = Number(val);
+    const [order, num] = orders[i];
 
-    if (order == 'push_front') {
-      headIdx -= 1;
-      deque[headIdx] = num;
-    }
-
-    if (order == 'push_back') {
-      deque[tailIdx] = num;
-      tailIdx += 1;
-    }
-
-    if (order == 'pop_front') {
-      if (headIdx === tailIdx) {
-        result.push(-1);
-      } else {
-        result.push(deque[headIdx]);
-        delete deque[headIdx];
-        headIdx += 1;
-      }
-    }
-
-    if (order == 'pop_back') {
-      if (headIdx === tailIdx) {
-        result.push(-1);
-      } else {
-        tailIdx -= 1;
-        result.push(deque[tailIdx]);
-        delete deque[tailIdx];
-      }
-    }
-
-    if (order == 'size') {
-      result.push(tailIdx - headIdx);
-    }
-
-    if (order == 'empty') {
-      result.push(headIdx === tailIdx ? 1 : 0);
-    }
-
-    if (order == 'front') {
-      if (headIdx === tailIdx) {
-        result.push(-1);
-      } else {
-        result.push(deque[headIdx]);
-      }
-    }
-
-    if (order == 'back') {
-      if (headIdx === tailIdx) {
-        result.push(-1);
-      } else {
-        result.push(deque[tailIdx - 1]);
-      }
+    switch (order) {
+      case 'push_front':
+        deque.unshift(num);
+        break;
+      case 'push_back':
+        deque.push(num);
+        break;
+      case 'pop_front':
+        result.push(deque.length === 0 ? -1 : deque.shift());
+        break;
+      case 'pop_back':
+        result.push(deque.length === 0 ? -1 : deque.pop());
+        break;
+      case 'size':
+        result.push(deque.length);
+        break;
+      case 'empty':
+        result.push(deque.length === 0 ? 1 : 0);
+        break;
+      case 'front':
+        result.push(deque.length === 0 ? -1 : deque[0]);
+        break;
+      case 'back':
+        result.push(deque.length === 0 ? -1 : deque[deque.length - 1]);
+        break;
     }
   }
 
