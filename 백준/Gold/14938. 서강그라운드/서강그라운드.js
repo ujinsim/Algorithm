aac2b2d37143a0808c3aea6c6b8a748d2e454542
ujinsim <syj11243@naver.com>
2026-01-14 19:08:22 +0900

    [Gold IV] Title: 서강그라운드, Time: 160 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.js
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.js
@@ -3,68 +3,54 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim();
 
 function solution(input) {
-  // 선 위에 숫자는 수색 범위  체크용이다
-  // 다익스트라로 풀기 + 플로이드 워셜
-  // n번에 떨어진 경우로 각각 돌기 << 이때 수색범위보다 작으면 업데이트
-  // 행별 맥스를 구해서 최대가 정답
-
   const parseInput = input.split(`\n`);
   const [N, M, R] = parseInput[0].split(' ').map(Number);
   const countes = parseInput[1].split(' ').map(Number);
   const nodes = parseInput.slice(2).map(a => a.split(' ').map(Number));
 
-  const map = new Map();
-  const dists = Array.from({ length: N + 1 }, () =>
+  // 각 그래프에서의 거리를 구하고 마지막에 각 노드별 합(수색범위 이내)을 더한 후 최대를 찾는다
+  const dist = Array.from({ length: N + 1 }, () =>
     new Array(N + 1).fill(Infinity)
   );
 
   for (let i = 0; i < nodes.length; i++) {
     const [n, v, c] = nodes[i];
-    if (!map.has(n)) map.set(n, []);
-    map.get(n).push([v, c]);
 
-    if (!map.has(v)) map.set(v, []);
-    map.get(v).push([n, c]);
-
-    dists[n][v] = c;
-    dists[v][n] = c;
+    dist[n][v] = c;
+    dist[v][n] = c;
   }
 
-  for (let i = 0; i <= N; i++) {
-    for (let j = 0; j <= N; j++) {
-      if (i == j) {
-        dists[i][j] = 0;
-      }
-    }
+  for (let i = 1; i <= N; i++) {
+    dist[i][i] = 0;
   }
 
-  for (let k = 1; k < N + 1; k++) {
-    for (let i = 1; i < N + 1; i++) {
-      for (let j = 1; j < N + 1; j++) {
-        if (i !== j) {
-          if (dists[i][k] + dists[k][j] < dists[i][j]) {
-            dists[i][j] = dists[i][k] + dists[k][j];
+  for (let k = 1; k <= N; k++) {
+    for (let i = 1; i <= N; i++) {
+      for (let j = 1; j <= N; j++) {
+        if (dist[i][k] !== Infinity && dist[k][i] !== Infinity && i !== j) {
+          if (dist[i][k] + dist[k][j] < dist[i][j]) {
+            dist[i][j] = dist[i][k] + dist[k][j];
           }
         }
       }
     }
   }
 
-  let result = 0;
+  let maxResult = -Infinity;
 
-  for (let i = 1; i < N + 1; i++) {
+  for (let i = 1; i <= N; i++) {
     let count = 0;
-    for (let j = 1; j < N + 1; j++) {
-      if (dists[i][j] !== Infinity && dists[i][j] <= M) {
+    for (let j = 1; j <= N; j++) {
+      if (dist[i][j] <= M) {
         count += countes[j - 1];
       }
     }
-    if (count > result) {
-      result = count;
+    if (count > maxResult) {
+      maxResult = count;
     }
   }
 
-  return result;
+  return maxResult;
 }
 
 console.log(solution(input));
