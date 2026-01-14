a4e0a8aa80cf34bc2e44cc141938dcd8a6c32403
ujinsim <syj11243@naver.com>
2026-01-14 19:41:36 +0900

    [Gold IV] Title: 명제 증명, Time: 172 ms, Memory: 13536 KB -BaekjoonHub

--- 백준/Gold/2224. 명제 증명/명제 증명.js
+++ 백준/Gold/2224. 명제 증명/명제 증명.js
@@ -3,44 +3,43 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim();
 
 function solution(input) {
-  const lines = input.split('\n');
-  const n = parseInt(lines[0]);
-  const edges = [];
+  const parseInput = input.split(`\n`);
+  const N = Number(parseInput[0]);
+  const nodes = parseInput.slice(1).map(a => a.split(' => '));
 
-  for (let i = 1; i <= n; i++) {
-    const [from, to] = lines[i].split('=>').map(s => s.trim());
-    edges.push([from, to]);
-  }
+  const dists = Array.from({ length: 52 }, () => Array(52).fill(false));
+  for (let i = 0; i < 52; i++) dists[i][i] = true;
 
-  const dist = Array.from({ length: 52 }, () => Array(52).fill(false));
-  for (let i = 0; i < 52; i++) dist[i][i] = true;
-
-  function getIdx(char) {
+  function charToIdx(char) {
     const code = char.charCodeAt(0);
-    if (code <= 90) return code - 65;
-    return code - 71;
-  }
-  function getChar(num) {
-    if (num < 26) {
-      return String.fromCharCode(num + 65);
+
+    if (code <= 90) {
+      return code - 65;
     } else {
-      return String.fromCharCode(num + 71);
+      return code - 71;
     }
   }
 
-  for (let i = 0; i < edges.length; i++) {
-    const [from, to] = edges[i];
+  function idxToChar(idx) {
+    if (idx <= 25) {
+      return String.fromCharCode(idx + 65);
+    } else {
+      return String.fromCharCode(idx + 71);
+    }
+  }
 
-    const x = getIdx(from);
-    const y = getIdx(to);
-    if (from === to) continue;
-    dist[x][y] = true;
+  for (let i = 0; i < nodes.length; i++) {
+    const [one, two] = nodes[i];
+    if (one === two) continue;
+    dists[charToIdx(one)][charToIdx(two)] = true;
   }
 
   for (let k = 0; k < 52; k++) {
     for (let i = 0; i < 52; i++) {
       for (let j = 0; j < 52; j++) {
-        if (dist[i][k] && dist[k][j]) dist[i][j] = true;
+        if (dists[i][k] && dists[k][j]) {
+          dists[i][j] = true;
+        }
       }
     }
   }
@@ -49,8 +48,8 @@ function solution(input) {
 
   for (let i = 0; i < 52; i++) {
     for (let j = 0; j < 52; j++) {
-      if (i !== j && dist[i][j] === true) {
-        result.push([`${getChar(i)} => ${getChar(j)}`]);
+      if (i !== j && dists[i][j] === true) {
+        result.push(`${idxToChar(i)} => ${idxToChar(j)}`);
       }
     }
   }
