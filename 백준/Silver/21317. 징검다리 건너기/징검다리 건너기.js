e3bf84979328ff2c545bc9686c98d47009829b97
ujinsim <syj11243@naver.com>
2026-01-20 09:15:14 +0900

    [Silver I] Title: 징검다리 건너기, Time: 92 ms, Memory: 9372 KB -BaekjoonHub

---
+++ 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
+
+function solution(inputData) {
+  const parseInput = inputData.trim().split('\n');
+  const N = Number(parseInput[0]);
+  if (N === 1) return 0;
+
+  const nums = parseInput.slice(1, N).map(a => a.split(' ').map(Number));
+  const K = Number(parseInput[parseInput.length - 1]);
+
+  const INF = Infinity;
+  const dp = Array.from({ length: N }, () => [INF, INF]);
+
+  dp[0][0] = 0;
+
+  if (N > 1) dp[1][0] = nums[0][0];
+  if (N > 2) dp[2][0] = Math.min(dp[1][0] + nums[1][0], nums[0][1]);
+
+  for (let i = 1; i < N; i++) {
+    if (i >= 1) {
+      dp[i][0] = Math.min(dp[i][0], dp[i - 1][0] + nums[i - 1][0]);
+    }
+    if (i >= 2) {
+      dp[i][0] = Math.min(dp[i][0], dp[i - 2][0] + nums[i - 2][1]);
+    }
+
+    if (i >= 1) {
+      dp[i][1] = Math.min(dp[i][1], dp[i - 1][1] + nums[i - 1][0]);
+    }
+    if (i >= 2) {
+      dp[i][1] = Math.min(dp[i][1], dp[i - 2][1] + nums[i - 2][1]);
+    }
+
+    if (i >= 3) {
+      dp[i][1] = Math.min(dp[i][1], dp[i - 3][0] + K);
+    }
+  }
+
+  return Math.min(dp[N - 1][0], dp[N - 1][1]);
+}
+
+console.log(solution(input));
