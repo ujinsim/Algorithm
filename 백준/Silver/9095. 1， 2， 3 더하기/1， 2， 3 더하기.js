b04ded64dc872ae4fbec2f111831f78f5296361f
ujinsim <syj11243@naver.com>
2026-01-20 19:52:44 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,33 +1,28 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
 
-function solution() {
-    const N = Number(input[0]);
-    const nums = input.slice(1).map(Number);
-    const maxTarget = Math.max(...nums);
-    const dp = new Array(maxTarget + 1).fill(0);
+function solution(input) {
+  const parseInput = input.split(`\n`);
+  const N = Number(parseInput[0]);
+  const nums = parseInput.slice(1).map(Number);
+  const dp = new Array(Math.max(...nums)).fill(0);
 
-    dp[1] = 1;
-    
-    if (maxTarget >= 2) {
-        dp[2] = 2;
-    }
-    
-    if (maxTarget >= 3) {
-        dp[3] = 4;
-    }
+  dp[0] = 1;
+  dp[1] = 2;
+  dp[2] = 4;
 
-    for (let i = 4; i <= maxTarget; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-    }
-    
-    const results = [];
-    for (let i = 0; i < N; i++) {
-        results.push(dp[nums[i]]); 
-    }
-    
+  for (let i = 3; i < dp.length; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
 
-    console.log(results.join('\n'));
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    result.push(dp[nums[i] - 1]);
+  }
+
+  return result.join(`\n`);
 }
 
-solution();
+console.log(solution(input));
