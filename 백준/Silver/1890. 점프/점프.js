b12ec1e422b0417fceb8218ba870e3fff0ff4358
ujinsim <syj11243@naver.com>
2026-01-22 09:15:35 +0900

    [Silver I] Title: 점프, Time: 92 ms, Memory: 9396 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.js
+++ 백준/Silver/1890. 점프/점프.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim();
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// const input = fs.readFileSync(filePath).toString().trim();
 
 function solution(input) {
   let result = 0;
@@ -14,15 +17,18 @@ function solution(input) {
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      const n = map[i][j];
-
-      if (dp[i][j] == BigInt(0) || (i == N - 1 && j === N - 1)) continue;
-
-      if (j + n < N) {
-        dp[i][j + n] += dp[i][j];
+      if (i == N - 1 && j == N - 1) {
+        break;
       }
-      if (i + n < N) {
-        dp[i + n][j] += dp[i][j];
+      if (dp[i][j] !== 0) {
+        const v = map[i][j];
+
+        if (v + i < N) {
+          dp[v + i][j] += dp[i][j];
+        }
+        if (v + j < N) {
+          dp[i][j + v] += dp[i][j];
+        }
       }
     }
   }
