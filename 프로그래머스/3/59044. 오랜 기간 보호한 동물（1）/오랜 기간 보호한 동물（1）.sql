e5ac5065a0b5354cfa17c2c7aabb45a453c3d45b
ujinsim <syj11243@naver.com>
2026-01-23 18:08:43 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,12 @@
+select name, datetime
+from ANIMAL_INS
+where animal_id not in (
+select animal_id
+from ANIMAL_OUTS
+)
+order by datetime asc
+limit 3
+
+# 아직 입양을 못 간 동물 중, 
+# 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 
+# 이때 결과는 보호 시작일 순으로 조회해야 합니다.
