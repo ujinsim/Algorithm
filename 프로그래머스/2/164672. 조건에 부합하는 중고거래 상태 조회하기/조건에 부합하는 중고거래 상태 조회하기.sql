7fb108b10f441ffb92dfefca70e38d799bfdf53e
ujinsim <syj11243@naver.com>
2026-01-26 16:50:51 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+select BOARD_ID,WRITER_ID,TITLE,PRICE,
+        (case 
+        when STATUS='SALE' then '판매중'
+        when STATUS='RESERVED' then '예약중'
+        when STATUS='DONE' then '거래완료'
+        end ) as STATUS
+        
+from USED_GOODS_BOARD
+where date_format(CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
+order by BOARD_ID desc
