f3229f8efa8bbbc92a8143133d4c9e931dce1863
ujinsim <syj11243@naver.com>
2026-02-01 12:22:25 +0900

    [level 3] Title: 순위, Time: 58.12 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,60 +1,58 @@
 function solution(n, results) {
-    // -1 -> 패배 , 1 -> 승리 
+   
+    //만약 A 선수가 B 선수보다 실력이 좋다면 A 선수는 B 선수를 항상 이깁니다
+    //정확하게 순위를 매길 수 있는 선수의 수를 return 하도록 
     
-    const map = Array.from({length:n+1}, ()=> new Array(n+1).fill(Infinity))
+    // 다익스트라 
     
-    for(let i=0; i<results.length; i++){
-        const [a,b] = results[i]
-        map[a][b] = 1
-        map[b][a] = -1
+    // 누구한테 졌는지를 다 나타내고 -> 나를 제외한 칸이 채워져있다면 등수를 알 수 있음 
+    // 추가적으로 알 수 있는 정보가 있는지 확인 
+    
+    const graph = Array.from({length:n}, ()=> new Array(n).fill(Infinity))
+    
+    // [a,b] (이긴(1), 진(-1)) -> i가 k한테 지고 k가 j한테 졌으면 i도 j한테 짐 
+    
+    for(let [w,l] of results){
+        graph[w-1][l-1] = 1
+        graph[l-1][w-1] = -1
     }
     
-    // 둘다 1이면 역방향은 -1 
-    
-    for(let i=1; i<=n; i++){
-        map[i][i] = 0
-}
-    
-    for(let k=1; k<=n; k++){
-        for(let i=1; i<=n; i++){
-            for(let j=1; j<=n; j++){
-                if(map[i][k] == 1 && map[k][j] == 1){
-                    map[i][j] = 1
-                }
-                if(map[i][k] == -1 && map[k][j] == -1){
-                     map[i][j] = -1
-                }
-            }
-        }
-    }
-    const order = new Array(n+1).fill(Infinity)
-    
-     for(let i=1; i<=n; i++){
-         let isInfinity = false
-         let count = 1
-            for(let j=1; j<=n; j++){
-                if(map[i][j] == Infinity){
-                    isInfinity= true
-                }
-                else{
-                    if(map[i][j] == -1){
-                        count +=1
+    for(let k=0; k<n; k++){
+        for(let i=0; i<n; i++){
+            for(let j=0; j<n; j++){
+                if(i!==j){
+                    if(graph[i][k] == -1 && graph[k][j] == -1){
+                        graph[i][j] = -1
+                    }
+                    
+                     if(graph[i][k] == 1 && graph[k][j] == 1){
+                        graph[i][j] = 1
                     }
                 }
             }
-         if(!isInfinity){
-             order[i] = count
-         }
-        }
-    
-    let result = 0
-    
-    for(let i=0; i<order.length; i++){
-        if(order[i] !== Infinity){
-            result+=1
         }
     }
     
-    return result
+    let count = 0
+    
+    for(let i=0; i<n; i++){
+        let cur = 0
+        
+        for(let j=0; j<n; j++){
+            if(graph[i][j] !== Infinity){
+                cur+=1
+            }
+        }
+        
+        if(cur == n-1){
+            count +=1
+        }
+    }
+    
+    
+    for(let g of graph){
+        console.log(g)
+    }
 
+    return count
 }
