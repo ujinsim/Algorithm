e27e293800cd6e41c3bd32e3fb6a0c36482039f7
ujinsim <syj11243@naver.com>
2026-02-01 22:00:32 +0900

    [Silver II] Title: A → B, Time: 92 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -1,22 +1,39 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
 
 function solution(input) {
-  let [initial, target] = input.split(" ").map(Number);
-  let count = 1; 
+  const [A, B] = input.split(' ').map(Number);
 
-  while (initial < target) {
-    if (target % 10 === 1) {
-      target = Math.floor(target / 10);
-    } else if (target % 2 === 0) {
-      target = target / 2;
-    } else {
-      return -1; 
+  //B를 A로 바꾸면 될듯
+  // 1이면 뒤에 빼고 아니면 2를 나눈다
+
+  function recursive(cur, count) {
+    if (cur == A) {
+      return count;
     }
-    count++;
+
+    if (cur < A) {
+      return -1;
+    }
+
+    const strCur = String(cur);
+
+    const lastChar = strCur.at(-1);
+
+    if (lastChar === '1') {
+      const next = Number(strCur.slice(0, -1));
+      return recursive(next, count + 1);
+    }
+
+    if (cur % 2 == 0) {
+      return recursive(cur / 2, count + 1);
+    }
+
+    return -1;
   }
 
-  return target === initial ? count : -1;
+  return recursive(B, 1);
 }
 
-
-console.log(solution(input));  
+console.log(solution(input));
