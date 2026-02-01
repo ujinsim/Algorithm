4b8eda43d99afb7cf06f15944fba1df4161cba9b
ujinsim <syj11243@naver.com>
2026-02-01 22:13:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,13 +1,25 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
 
 function solution(input) {
-  const part = input.split("-");
+  // 빼기 기호 뒤에는 다 더해 최대로 만들어준다
 
-  const result = part.reduce((total, item, idx) => {
-    const sum = item.split("+").map(a => +a).reduce((a, b) => a + b, 0);
+  const parse = input.split('-');
 
-    return idx === 0 ? sum : total - sum;
-  }, 0);
+  let result = 0;
+
+  for (let i = 0; i < parse.length; i++) {
+    const nums = parse[i].split('+').map(Number);
+
+    const sum = nums.reduce((a, b) => a + b);
+
+    if (i == 0) {
+      result += sum;
+    } else {
+      result -= sum;
+    }
+  }
 
   return result;
 }
