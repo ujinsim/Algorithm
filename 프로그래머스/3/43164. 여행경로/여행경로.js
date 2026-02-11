edb53888053db1a605df7ee8c1a76ef003b600d5
ujinsim <syj11243@naver.com>
2026-02-11 15:36:33 +0900

    [level 3] Title: 여행경로, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,52 +1,42 @@
 function solution(tickets) {
-    const targetCount = tickets.length + 1;
-    const ticketsMap = new Map();
+   tickets.sort((a,b)=> {
+       if(a[1] == b[1]){
+           return a[0] < b[0] ? -1 : 1
+       }
+       return a[1] < b[1] ? -1 : 1
+   })
     
-    for(let i = 0; i < tickets.length; i++) {
-        const [start, end] = tickets[i];
-        if(!ticketsMap.has(start)) ticketsMap.set(start, []);
-        ticketsMap.get(start).push({ destination: end, index: i, used: false });
-    }
+   const visited = new Array(tickets.length).fill(false)
+   let dist = []
+   const result = []
     
-
-    for(const destinations of ticketsMap.values()){
+   function backtrack(node, count){
     
-        destinations.sort((a, b) => a.destination.localeCompare(b.destination));
-    }
-    
-    const queue = [
-        ["ICN", ["ICN"], new Set()]
-    ];
-
-    while(queue.length){
-        const [current, path, usedTickets] = queue.shift();
-        
        
-        if(path.length === targetCount) {
-            return path;
-        }
-        
-        const possibleDestinations = ticketsMap.get(current) || [];
-        
-        for(let i = 0; i < possibleDestinations.length; i++){
-            const ticket = possibleDestinations[i];
-            
-            if(!usedTickets.has(ticket.index)){
-                
-              
-                const newUsedTickets = new Set(usedTickets);
-                newUsedTickets.add(ticket.index);
-                
-    
-                const newPath = [...path, ticket.destination];
-                
+       if(result.length >= 1) return
+       
+       if(count == tickets.length+1){
+           result.push(...dist)
+           return 
+       }
+       
+       for(let i=0; i<tickets.length; i++){
+           const [start,end] = tickets[i]
            
-                queue.push([ticket.destination, newPath, newUsedTickets]);
-               
-            }
-        }
-    }
+           if(start == node && !visited[i]){
+               visited[i] = true
+               dist.push(end)
+               backtrack(end, count+1)
+               dist.pop(end)
+               visited[i] = false
+              
+           }
+       }
+   }
     
-
-    return [];
+   dist.push("ICN")
+   backtrack("ICN",1)
+    
+    return result
+  
 }
