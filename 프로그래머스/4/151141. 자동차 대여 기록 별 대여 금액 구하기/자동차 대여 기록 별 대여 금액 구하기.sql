9c2d976cafbb8ff333761703ee7cabd9b33e39b5
ujinsim <syj11243@naver.com>
2026-02-20 20:59:28 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,20 +1,14 @@
-
-with h as (
-select car_id, history_id, start_date, end_date,
-    (case 
-        when timestampdiff(day,start_date, end_date)+1 >= 90 then '90일 이상'
-        when timestampdiff(day,start_date, end_date)+1 >= 30 then '30일 이상'
-        when timestampdiff(day, start_date, end_date) + 1 >= 7 then '7일 이상'
-        else '미적용' end)
-        as duration
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-)
-
-select h.HISTORY_ID, 
-        floor(c.daily_fee * (timestampdiff(day, h.start_date, h.end_date) + 1) * (100 - ifnull(p.discount_rate, 0)) / 100) as fee
+select d.history_id, round((d.diff_day * d.daily_fee) * (1- ifnull(p.discount_rate,0) / 100),0) as fee
+from (select c.car_type, c.car_id, c.daily_fee, h.history_id, (case
+        when timestampdiff(day, h.start_date, h.end_date) +1 >= 90 then '90일 이상'
+        when timestampdiff(day, h.start_date, h.end_date) +1 >= 30 then '30일 이상'
+        when timestampdiff(day, h.start_date, h.end_date) +1 >= 7 then '7일 이상'
+        else null end ) as duration_type,
+        (timestampdiff(day, h.start_date, h.end_date) +1) as diff_day
 from CAR_RENTAL_COMPANY_CAR as c
-join h on c.car_id = h.car_id
-left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p 
-     on p.car_type = c.car_type and h.duration = p.DURATION_TYPE
-where c.car_type = '트럭'
-order by fee desc, h.HISTORY_ID desc;
+join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h on h.car_id = c.car_id
+where c.car_type = '트럭') as d
+left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p on d.duration_type = p.duration_type
+and d.car_type = p.car_type 
+
+order by fee desc, d.history_id desc
