953f3f2f33fe4c9932a6b3951222faedff309079
ujinsim <syj11243@naver.com>
2026-02-21 11:10:46 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,17 +1,17 @@
-with recursive a as (
+with recursive a as(
     select id, parent_id, 1 as generation
-    from ECOLI_DATA
-    where parent_id is null
+    from ecoli_data
+    where parent_id is null 
     
-    union all 
+    union all
     
-    select b.id, b.parent_id, a.generation+1 as generation
-    from ECOLI_DATA as b
-    join a on a.id = b.parent_id
+    select b.id, b.parent_id, generation + 1 as generation
+    from a 
+    join ecoli_data as b on b.parent_id = a.id
 )
 
-select count(*) as count, generation 
-from a
-where id not in (select distinct(b.parent_id) from a as b where b.parent_id is not null)
-group by generation
+select count(*) as count, generation
+from a 
+where id not in (select parent_id from a where parent_id is not null)
+group by generation 
 order by generation
