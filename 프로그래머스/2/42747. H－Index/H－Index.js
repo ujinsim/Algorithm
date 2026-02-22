709f7638af665557984dfa824e1b5115b08226b0
ujinsim <syj11243@naver.com>
2026-02-22 16:23:05 +0900

    [level 2] Title: H-Index, Time: 3.45 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,23 +1,40 @@
 function solution(citations) {
-    let i = citations.length; 
+  // 이분탐색으로 간다 ㅅㄱ 
+    
+    
+    let low = 0 
+    let high = Math.max(...citations)
+    let n = citations.length
+    let result
+    
+    // n회 이상 인용되지 못했다면 high를 줄인다 
+    
+    while(low <= high){
+        let mid = Math.floor((low+high)/2)
+        
+        let count = 0
+        let left = 0
 
-  
-    while(i >= 0){ 
-        let count = 0;
-
-        for(let k = 0; k < citations.length; k++){
-            if(citations[k] >= i){
-                count += 1;
-               
-                if(count >= i){
-                    return i;
-                }
+        for(let i=0; i<n; i++){
+            if(citations[i] >= mid){
+                count +=1 
             }
-
-          
+            else {
+                left +=1
+            }
         }
-
-        i -= 1; 
+        
+        console.log(mid, left, count)
+        
+        if(count >= mid && left <= mid){
+            low = mid + 1
+            result = mid
+        }
+        else {
+            high = mid - 1
+        }
     }
-    return 0;
+    
+    return result
+
 }
