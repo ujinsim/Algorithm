b4da72a4d4baacda0d9e95e93cb4ca10d8932a57
ujinsim <syj11243@naver.com>
2026-02-22 15:14:56 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,14 +1,25 @@
-select d.history_id, round((d.diff_day * d.daily_fee) * (1- ifnull(p.discount_rate,0) / 100),0) as fee
-from (select c.car_type, c.car_id, c.daily_fee, h.history_id, (case
-        when timestampdiff(day, h.start_date, h.end_date) +1 >= 90 then '90일 이상'
-        when timestampdiff(day, h.start_date, h.end_date) +1 >= 30 then '30일 이상'
-        when timestampdiff(day, h.start_date, h.end_date) +1 >= 7 then '7일 이상'
-        else null end ) as duration_type,
-        (timestampdiff(day, h.start_date, h.end_date) +1) as diff_day
+with a as(
+select h.history_id, c.daily_fee, c.car_id, c.car_type,
+    (case
+        when timestampdiff(day, h.start_date, h.end_date) + 1 >= 90 then '90일 이상'
+     when timestampdiff(day, h.start_date, h.end_date) + 1 >= 30 then '30일 이상'
+     when timestampdiff(day, h.start_date, h.end_date) + 1 >= 7 then '7일 이상'
+     else null end
+    ) as duration_type, 
+    (timestampdiff(day, h.start_date, h.end_date) + 1) as diff
 from CAR_RENTAL_COMPANY_CAR as c
-join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h on h.car_id = c.car_id
-where c.car_type = '트럭') as d
-left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p on d.duration_type = p.duration_type
-and d.car_type = p.car_type 
+join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h on h.car_id = c.car_id and 
+    c.car_type = '트럭'
+)
 
-order by fee desc, d.history_id desc
+
+select history_id , round((diff * daily_fee) - ((diff * daily_fee) * (1/100) * ifnull(discount_rate,0)),0) as fee
+from a
+left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p on a.duration_type = p.duration_type and a.car_type = p.car_type
+order by fee desc, history_id desc
+
+# // 트럭인거 뽑기 
+# // duration _ type 뽑기 , diff day 뽑기 
+
+
+# // 마지막 테이블이랑 조인해서 결과 뽑기 
