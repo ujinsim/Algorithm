39d7d8fa40a0a14025cf4c3e40233757d487efc2
ujinsim <syj11243@naver.com>
2026-02-22 15:05:52 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,6 +1,8 @@
-select DISTINCT d.id, d.email, d.first_name, d.last_name
-from DEVELOPERS as d, (select code from SKILLCODES where category = 'Front End') as s
-where (d.skill_code & s.code) = s.code
-order by d.id
-
--- WHERE (D.SKILL_CODE & S.CODE) = S.CODE
+select id, email, first_name, last_name
+from developers 
+where skill_code & (
+    select sum(code)
+    from SKILLCODES
+    where category = 'Front End'
+) 
+order by id 
