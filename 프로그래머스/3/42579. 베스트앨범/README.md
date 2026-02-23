5b84c570b58d866806f89babc1c22afb70dba941
ujinsim <syj11243@naver.com>
2026-02-23 12:05:39 +0900

    [level 3] Title: 베스트앨범, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?gad_source=1&gad_campaignid=23037984604&gbraid=0AAAAAC_c4nCJ6vT4w-gM0l-m9OKdeH9Tf&gclid=Cj0KCQiA7-rMBhCFARIsAKnLKtBkN6tTV2MyIEyvXFJF-nvoLaOiBtP_Gc1gLg097yktTxxU4pK1LYcaAvE2EALw_wcB) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 5.23 ms
+메모리: 33.5 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 19일 14:55:09
+2026년 02월 23일 12:05:36
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -9,36 +9,31 @@ function solution(genres, plays) {
     const play = plays[i]
     if(!map.has(genre)) map.set(genre,[])
      map.get(genre).push([play,i])
+     
+     if(!count.has(genre)) count.set(genre, 0)
+     count.set(genre, count.get(genre) + play)
+    
  }
     
- for(const [key, value] of map){
-    value.sort((a,b)=> b[0]-a[0])
- }   
+    
+ const arr = [...count.entries()].sort((a,b)=> b[1]-a[1])
  
-  for(let i=0; i<genres.length; i++){
-    const genre = genres[i]
-    const play = plays[i]
-    if(!count.has(genre)) count.set(genre,0)
-     count.set(genre, count.get(genre)+play)
- }
-    
- for(const [key, value] of map){
-     console.log(key, value)
- }
-    
  
-  const countArr = Array.from(count)
-  countArr.sort((a,b)=> b[1]-a[1])
+ for(let i of arr){
+     const [g,p] = i
+     
+     const sortedArr = map.get(g).sort((a,b)=> b[0]-a[0])
+     
+     for(let k=0; k<2; k++){
+         if(sortedArr[k]){
+             result.push(sortedArr[k][1])
+         }
+     }
+ }
+ 
+
+ 
+ // 총 합이 가장 큰거별로 두개 
     
-  for(let i=0; i<countArr.length; i++){
-      const genre = countArr[i][0]
-      const sortedCounts = map.get(genre)
-      
-      for(let j=0; j < Math.min(sortedCounts.length, 2); j++){
-          result.push(sortedCounts[j][1])
-      }
-  }
-    return result
+ return result
 }
-
-
