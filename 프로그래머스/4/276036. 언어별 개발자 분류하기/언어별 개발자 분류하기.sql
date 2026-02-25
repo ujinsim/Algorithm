98f6ed51a75c3324600c1bf74f9f12c392ef631c
ujinsim <syj11243@naver.com>
2026-02-26 07:28:04 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,12 +1,12 @@
-select grade, id, email
-from (select 
-(case
- when (skill_code & (select sum(code) from skillcodes where category = 'Front End')) and 
-      (skill_code & (select code from skillcodes where name = 'Python')) then 'A'
- when skill_code & (select code from skillcodes where name = 'C#') then 'B'
- when (skill_code & (select sum(code) from skillcodes where category = 'Front End')) then 'C'
-end ) as grade, id, email
-from DEVELOPERS
-) as d
-where d.grade is not null
-order by grade, id
+select *
+from (select (case
+       when skill_code & (select sum(code) from skillcodes where category='Front End' ) and 
+            skill_code & (select code from skillcodes where name='Python') then 'A'
+        
+        when skill_code & (select code from skillcodes where name='C#') then 'B'
+        when skill_code & (select sum(code) from skillcodes where category='Front End') then 'C'
+        end
+       ) as GRADE, ID, EMAIL
+from developers 
+order by grade,id) as g
+where grade is not null
