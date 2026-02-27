46c2ccafd2531dad93e5629ff5627f9350363afb
ujinsim <syj11243@naver.com>
2026-02-27 21:31:53 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,10 +1,15 @@
 function solution(strings, n) {
     strings.sort((a, b) => {
-      if (a[n] > b[n]) return 1;
-
-      else if (a[n] < b[n]) return -1;
- 
-      else return a > b ? 1 : -1;
+      if(a[n] > b[n]) {return 1}
+        
+        if(a[n] < b[n]) {
+            return -1
+        }
+        
+        if(a[n] == b[n]){
+           if(a>b) return 1
+           if(a<b) return -1
+        }
     });
     
     return  strings
