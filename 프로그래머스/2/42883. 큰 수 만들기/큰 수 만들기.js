1232ba0fb7141536c2f3a8891754406b31a264d5
ujinsim <syj11243@naver.com>
2026-02-27 15:07:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 57.45 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,27 +1,22 @@
 function solution(number, k) {
     const stack = []
-    
     let count = 0
+    
+    // 앞에 나련보다 작은수 있으면 count만 큼 ㄲㅈ세요 -> 순서 유지를 위해 스택 맨뒤만 
+    // 만약에 9876이여서 하나도 안꺼졌다면 count만큼 slice해주깅 ㅋ
     
     for(let i=0; i<number.length; i++){
         let cur = number[i]
         
-        while(stack[stack.length-1] < cur && count < k && stack.length > 0){
+        while(stack.length > 0 && stack[stack.length-1] < cur && count < k){
             stack.pop()
-            count+=1
+            count ++
         }
+        
         stack.push(cur)
     }
     
-    // k가 number보다 크다면 ? 
     
-    // count가 안제거 됐다면? 뒤에서 슬라이스 해주기 
-    // 23121 인데 4개 제거 넣어야 count가 늘어나니까 
-    // 9999999 이러면 
-    // 제거가 안되니까 제거안된거만큼 뒤에서 제거해주기 
-    
-
-    
-    return stack.slice(0, number.length - k).join("");
+    return stack.slice(0,number.length-k).join("")
     
 }
