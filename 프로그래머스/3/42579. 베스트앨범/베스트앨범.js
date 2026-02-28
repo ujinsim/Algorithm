637105bb0a4165c0f9aba39fd457278852e6985a
ujinsim <syj11243@naver.com>
2026-02-28 10:25:44 +0900

    [level 3] Title: 베스트앨범, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,39 +1,40 @@
 function solution(genres, plays) {
-
- const map = new Map()
- const count = new Map()
- const result = []
- 
- for(let i=0; i<genres.length; i++){
-    const genre = genres[i]
-    const play = plays[i]
-    if(!map.has(genre)) map.set(genre,[])
-     map.get(genre).push([play,i])
-     
-     if(!count.has(genre)) count.set(genre, 0)
-     count.set(genre, count.get(genre) + play)
+    const genresCount = new Map()
+    const cbg = new Map()
+    const n = genres.length
+    const result = []
     
- }
+    for(let i=0; i<n; i++){
+        let genre = genres[i]
+        let play = plays[i]
+        
+        if(!genresCount.has(genre)) genresCount.set(genre,0)
+        genresCount.set(genre,genresCount.get(genre) + play)
+        
+        if(!cbg.has(genre)) cbg.set(genre,[])
+        cbg.get(genre).push([play,i])
+    }
+    
+    const sg = [...genresCount].sort((a,b)=> b[1]-a[1])
+    
+    for(let i of sg){
+        const gen = i[0]
+        
+       const counts = cbg.get(gen).sort((a, b) => {
+        if (a[0] !== b[0]) {
+            return b[0] - a[0]; // 1. 재생 횟수 내림차순 (큰 게 위로)
+        }
+        return a[1] - b[1];     // 2. 재생 횟수 같으면 인덱스 오름차순 (작은 게 위로)
+    });
+        
+        for(let i=0; i<2; i++){
+            if(counts[i]){
+            result.push(counts[i][1])
+            }
+        }
+        
+    }
     
     
- const arr = [...count.entries()].sort((a,b)=> b[1]-a[1])
- 
- 
- for(let i of arr){
-     const [g,p] = i
-     
-     const sortedArr = map.get(g).sort((a,b)=> b[0]-a[0])
-     
-     for(let k=0; k<2; k++){
-         if(sortedArr[k]){
-             result.push(sortedArr[k][1])
-         }
-     }
- }
- 
-
- 
- // 총 합이 가장 큰거별로 두개 
-    
- return result
+    return result
 }
