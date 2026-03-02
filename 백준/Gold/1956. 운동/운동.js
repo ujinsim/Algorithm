0c34e38a6d1a139b1d0c91139c42722076ca7482
ujinsim <syj11243@naver.com>
2026-03-02 18:58:53 +0900

    [Gold IV] Title: 운동, Time: 800 ms, Memory: 74248 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.js
+++ 백준/Gold/1956. 운동/운동.js
@@ -5,45 +5,42 @@ const input = fs.readFileSync(filePath).toString().trim();
 function solution(input) {
   const parseInput = input.split(`\n`);
   const [V, E] = parseInput[0].split(' ').map(Number);
-  const edges = parseInput.slice(1).map(a => a.split(' ').map(Number));
+  const nums = parseInput.slice(1).map(a => a.split(' ').map(Number));
 
-  const dist = Array.from({ length: V + 1 }, () =>
+  // 1->3 3->1 이게 가장 직은거 플로이드 우 ㅓ셜 단뱡향
+  // 플로이드 워셜은 작은거있으면 업댓해줘야됭
+
+  const dists = Array.from({ length: V + 1 }, () =>
     new Array(V + 1).fill(Infinity)
   );
 
-  for (let i = 0; i < edges.length; i++) {
-    const [n, v, c] = edges[i];
+  for (let [n, v, c] of nums) {
+    dists[n][v] = Math.min(dists[n][v], c);
+  }
 
-    dist[n][v] = c;
+  for (let i = 1; i <= V; i++) {
+    dists[i][i] = 0;
   }
 
   for (let k = 1; k <= V; k++) {
     for (let i = 1; i <= V; i++) {
       for (let j = 1; j <= V; j++) {
-        if (i !== j) {
-          if (dist[i][k] + dist[k][j] < dist[i][j]) {
-            dist[i][j] = dist[i][k] + dist[k][j];
-          }
-        }
+        dists[i][j] = Math.min(dists[i][k] + dists[k][j], dists[i][j]);
       }
     }
   }
 
-  let minCycle = Infinity;
+  let result = Infinity;
 
   for (let i = 1; i <= V; i++) {
     for (let j = 1; j <= V; j++) {
       if (i !== j) {
-        if (dist[i][j] !== Infinity && dist[j][i] !== Infinity) {
-          if (dist[i][j] + dist[j][i] < minCycle) {
-            minCycle = dist[i][j] + dist[j][i];
-          }
-        }
+        result = Math.min(result, dists[i][j] + dists[j][i]);
       }
     }
   }
 
-  return minCycle == Infinity ? -1 : minCycle;
+  return result == Infinity ? -1 : result;
 }
 
 console.log(solution(input));
