20d6ee84f22fabdd725d944683e3bc2b94c3a9ca
ujinsim <syj11243@naver.com>
2026-03-02 16:04:19 +0900

    [level 4] Title: 사칙연산, Time: 33.90 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.js
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.js
@@ -1,32 +1,37 @@
 function solution(arr) {
-    const N = Math.ceil(arr.length/2)
-    const min_dp = Array.from({length:N}, ()=> new Array(N).fill(Infinity))
-    const max_dp = Array.from({length:N}, ()=> new Array(N).fill(-Infinity))
+    // 계산중 최댓값 
+    const n = Math.floor((arr.length/2)) + 1
+    const max_dp = Array.from({length:n}, ()=> new Array(n).fill(-Infinity))
+    const min_dp = Array.from({length:n}, ()=> new Array(n).fill(Infinity))
     
-    for (let i = 0; i < N; i++) {
-        max_dp[i][i] = Number(arr[i * 2]);
-        min_dp[i][i] = Number(arr[i * 2]);
+    for(let i=0; i<arr.length; i++){
+        if(i%2 == 0){
+            max_dp[i/2][i/2] = Number(arr[i])
+            min_dp[i/2][i/2] = Number(arr[i])
+        }
     }
     
-    for(let gap=1; gap<N; gap++){
-        for(let i=0; i<N-gap; i++){
+    for(let gap=1; gap<n; gap++){
+        for(let i=0; i<n-gap; i++){
             let j = i+gap
             
             for(let k=i; k<j; k++){
-                const opr = arr[k*2 + 1]
+                const opr = arr[k*2+1]
                 
-                if(opr == '-'){
-                    min_dp[i][j] = Math.min(min_dp[i][j], min_dp[i][k] - max_dp[k+1][j])
-                    max_dp[i][j] = Math.max(max_dp[i][j], max_dp[i][k] - min_dp[k+1][j])
+                if(opr == '+'){
+                    max_dp[i][j] = Math.max(max_dp[i][k] + max_dp[k+1][j], max_dp[i][j])
+                    min_dp[i][j] = Math.min(min_dp[i][k] + min_dp[k+1][j], min_dp[i][j]) 
                 }
                 
-                else if(opr == '+'){
-                    min_dp[i][j] = Math.min(min_dp[i][j], min_dp[i][k] + min_dp[k+1][j])
-                    max_dp[i][j] = Math.max(max_dp[i][j], max_dp[i][k] + max_dp[k+1][j])
+                if(opr == '-'){
+                    max_dp[i][j] = Math.max(max_dp[i][k] - min_dp[k+1][j], max_dp[i][j])
+                    min_dp[i][j] = Math.min(min_dp[i][k] - max_dp[k+1][j], min_dp[i][j]) 
                 }
             }
         }
     }
     
-    return max_dp[0][N-1]
+    
+    return max_dp[0][n-1]
+    
 }
