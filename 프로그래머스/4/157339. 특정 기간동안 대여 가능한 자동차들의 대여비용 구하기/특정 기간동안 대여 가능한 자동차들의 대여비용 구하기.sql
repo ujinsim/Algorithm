eb193aa88ae6d567aebdf0c07c92bbd62793bb59
ujinsim <syj11243@naver.com>
2026-03-07 11:31:27 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,23 +1,18 @@
-with a as (
-
- select distinct(c.car_id), c.car_type, c.daily_fee
-from car_rental_company_car as c
-join car_rental_company_rental_history as h on c.car_id = h.car_id
-where c.car_type in ('세단','SUV') and 
-        h.car_id not in (select car_id
-                        from car_rental_company_rental_history
-                        where start_date <= '2022-11-30' and end_date >= '2022-11-01')
-
-
+with p as (
+    select *
+    from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    where car_type in ('세단', 'SUV') and duration_type = '30일 이상'
 )
- 
-select f.car_id, f.car_type, round(f.fee) as fee
-from (select a.car_id, a.car_type, (a.daily_fee * 30) - ((a.daily_fee * 30) * p.discount_rate * 1/100) as fee
-from CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
-join a on p.car_type = a.car_type
-where p.duration_type = '30일 이상') as f
 
-where f.fee >= 500000 and f.fee < 2000000
-
-order by fee desc, f.car_type, f.car_id desc
- 
+select distinct(c.car_id), c.car_type, round((30*c.daily_fee) - ((30*c.daily_fee) * p.discount_rate/100)) as fee
+from CAR_RENTAL_COMPANY_CAR as c
+join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h on c.car_id = h.car_id 
+join p on p.car_type = c.car_type
+where c.car_type in ('세단', 'SUV') and 
+      c.car_id not in ( select car_id
+                       from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                       where start_date < '2022-11-30' and end_date > '2022-11-01') and
+                  (30*c.daily_fee) - ((30*c.daily_fee) * p.discount_rate/100) >= 500000  and 
+                  (30*c.daily_fee) - ((30*c.daily_fee) * p.discount_rate/100) < 2000000
+                  
+order by fee desc, c.car_type, c.car_id desc
